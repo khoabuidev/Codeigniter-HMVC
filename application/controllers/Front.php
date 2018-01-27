e8176a17c0a8a18cc6eb7cd8909ed596e78196a5
Khoa Bui <khoa.bui.dev@gmail.com>
2018-01-27 21:29:37 +0700

    init source code

---
+++ application/controllers/Front.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Front extends MX_Controller {
+	public function __construct()
+	{
+		$this->load->helper('form');
+        $this->load->library('form_validation');	
+	}
+
+	public function index()
+	{
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		if ($this->form_validation->run() == FALSE) {
+                $this->load->view('form');
+        }
+        else {
+                $this->load->view('formsuccess');
+        }
+		
+	}
+}
---
+++ application/modules/block/controllers/Block.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Block extends MX_Controller {
+
+	public function hello()
+	{
+		return '<h1>Hello world</h1>';
+	}
+}
